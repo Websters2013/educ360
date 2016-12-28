97afd007e4a7acafafc15600a7b194527f6eff8b
mbomak <mboma@meta.ua>
2016-12-28 11:46:03 +0200

    footer

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,3 +1,53 @@
-( function(){
+"use strict";
 
-} )();
+( function () {
+
+  $( function() {
+
+    $.each( $( '.site__footer-scroll' ), function() {
+       
+      new  Scroll ( $( this ) );
+       
+    } );
+
+  } );
+
+
+  var Scroll = function ( obj ) {
+  
+    //private properties
+    var _self = this,
+        _btn = obj,
+        _html = $('body,html');
+
+    //private methods
+    var _constructor = function () {
+    
+           _onEvents();
+    
+        },
+    _onEvents = function () {
+    
+        _btn.on ( {
+            click: function () {
+             
+             _scrollToTop();
+
+            }
+        } );
+ 
+    },  
+    _scrollToTop = function () {
+
+      _html.animate({
+                 			scrollTop: 0
+             				}, 400);
+
+    }
+
+  
+    _constructor ();
+  
+  }
+ 
+} ) ();
